395a64c188b9e319e306188296e8c3e56fd270cb
igusvs <igoralves159@gmail.com>
2024-11-24 11:49:40 -0300

    include dynamodb

--- src/main.tf
+++ src/main.tf
@@ -12,10 +12,6 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "this" {
-  bucket = "bucket-terraform-shogun-backend"
-}
-
 resource "aws_db_instance" "shogun_data_base" {
 
 
@@ -102,4 +98,31 @@ resource "aws_security_group" "rds_sg" {
 
 data "aws_vpc" "this" {
   default = true
+}
+
+resource "aws_dynamodb_table" "clientes" {
+  name           = "clientes"
+  billing_mode   = "PAY_PER_REQUEST" 
+  hash_key       = "id_cliente"            
+
+  attribute {
+    name = "id_cliente"
+    type = "S" 
+  }
+
+  attribute {
+    name = "cpf"
+    type = "S" 
+  }
+
+  global_secondary_index {
+    name            = "cpf-index"
+    hash_key        = "cpf"       
+    projection_type = "ALL"        
+  }
+
+  tags = {
+    Environment = "dev"
+    Team        = "backend"
+  }
 }
